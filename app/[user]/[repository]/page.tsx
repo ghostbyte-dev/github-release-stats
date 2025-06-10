5617100f1bf873f3cad99f81e3243ca9d44e4458
Hiebeler <emanuel.hiebeler@gmail.com>
2025-06-10 19:42:24 +0200

    repo details design right bar finished

--- app/[user]/[repository]/page.tsx
+++ app/[user]/[repository]/page.tsx
@@ -3,6 +3,13 @@
 import useReleases from "@/hooks/useReleases";
 import useRepository from "@/hooks/useRepository";
 import { useParams } from "next/navigation";
+import Image from "next/image";
+import {
+	EyeIcon,
+	GitForkIcon,
+	LinkIcon,
+	StarIcon,
+} from "@phosphor-icons/react";
 
 export default function RepositoryDetails() {
 	const params = useParams();
@@ -15,23 +22,86 @@ export default function RepositoryDetails() {
 		repositoryName,
 	);
 
-	const [repository, isRepositoryPending, isRepositoryError] = useRepository(
-		user,
-		repositoryName,
-	);
-
-	if (isReleasesError || isRepositoryError) {
-		return <div>error</div>;
-	}
-
-	if (isReleasesPending || isRepositoryPending) {
-		return <div>Loading...</div>;
-	}
+	const [repository, isRepositoryPending] = useRepository(user, repositoryName);
 
 	return (
-		<div>
-			{repository ? <div>{repository.full_name}</div> : <></>}
-			{releases ? <>{releases.length}</> : <></>}
+		<div className="flex flex-col">
+			<div>
+				{repository ? (
+					<div className="flex flex-row gap-4">
+						<Image
+							src={repository.owner.avatar_url}
+							height={32}
+							width={32}
+							alt={"Avatar"}
+							className="rounded-full"
+						/>
+						<h1 className="text-4xl font-bold">{repository.full_name}</h1>
+					</div>
+				) : isRepositoryPending ? (
+					<>Loading...</>
+				) : (
+					<>an error occured</>
+				)}
+			</div>
+			<div className="flex flex-row mt-10">
+				<div className="flex-2/3 card">Diagramm</div>
+				<div className="flex-1/3">
+					{repository ? (
+						<div className="ml-8 mt-2 gap-1 flex flex-col">
+							<div className="flex flex-col gap-2">
+								<h3 className="font-bold text-xl">About</h3>
+								<p>{repository.description}</p>
+								<div className="flex flex-row gap-3 mt-1 text-sm">
+									<LinkIcon size={16} weight="bold" />
+									<a
+										href={repository.homepage}
+										className="text-link hover:underline font-bold"
+									>
+										{repository.homepage}
+									</a>
+								</div>
+							</div>
+							<a
+								className="flex flex-row mt-4 gap-3 hover:text-link text-secondary-text"
+								href={repository.stargazers_url}
+							>
+								<StarIcon size={18} />
+								<div className="flex flex-row gap-1 text-sm">
+									<p className="font-bold">{repository.stargazers_count}</p>
+									<p>stars</p>
+								</div>
+							</a>
+							<a
+								className="flex flex-row gap-3 hover:text-link text-secondary-text"
+								href={repository.watchers_url}
+								//watcher url not found
+							>
+								<EyeIcon size={18} />
+								<div className="flex flex-row gap-1 text-sm">
+									<p className="font-bold">{repository.watchers_count}</p>
+									<p>watchers</p>
+								</div>
+							</a>
+							<a
+								className="flex flex-row gap-3 hover:text-link text-secondary-text"
+								href={repository.forks_url}
+							>
+								<GitForkIcon size={18} />
+								<div className="flex flex-row gap-1 text-sm">
+									<p className="font-bold">{repository.forks}</p>
+									<p>forks</p>
+								</div>
+							</a>
+						</div>
+					) : isRepositoryPending ? (
+						<>Loading...</>
+					) : (
+						<>an error occured</>
+					)}
+				</div>
+			</div>
+			<div>Releases</div>
 		</div>
 	);
 }
--- types/repository.ts
+++ types/repository.ts
@@ -4,11 +4,16 @@ export type repository = {
 	id: number;
 	name: string;
 	full_name: string;
+	description: string;
+	homepage: string;
 	owner: User;
 	private: boolean;
 	fork: false;
 	html_url: string;
 	forks: number;
+	forks_url: string;
 	stargazers_count: number;
+	stargazers_url: string;
 	watchers_count: number;
+	watchers_url: string;
 };
