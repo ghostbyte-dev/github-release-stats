e655ce6194c338861ad4d9df80a34d833d7c9e7a
Hiebeler <emanuel.hiebeler@gmail.com>
2025-06-21 16:23:50 +0200

    change navbar background color

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <div className="border-b border-border px-5 py-5">
+    <div className="border-b border-border px-5 py-5 bg-background-dark">
       <Link href="/" className="">
         <span className="font-semibold">Github Release Stats</span>
       </Link>
