3730003cc90bd4f4ee995ad1df7d457def36965e
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-06-24 23:48:07 +0200

    Adjust paddings

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <div className="border-b border-border px-5 py-3 bg-background-dark flex">
+    <div className="border-b border-border px-2 lg:px-6 py-3 bg-background-dark flex">
       <Link href="/" className="">
         <div className="flex space-x-3 items-center p-2">
-          <Image src="/logo.svg" width={32} height={32} className="h-12 w-12 rounded-lg" alt="" />
+          <Image src="/logo.svg" width={48} height={48} className="h-12 w-12 rounded-lg" alt="" />
 
           <div>
             <span className="font-semibold text-xl">Github Release Stats</span>
