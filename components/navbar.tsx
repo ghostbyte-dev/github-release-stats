bcac0939270e79bde0ad9395b17fa9c1930b3115
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-06-24 23:20:20 +0200

    Add logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,20 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <div className="border-b border-border px-5 py-5 bg-background-dark">
+    <div className="border-b border-border px-5 py-3 bg-background-dark flex">
       <Link href="/" className="">
-        <span className="font-semibold text-xl">Github Release Stats</span>
+        <div className="flex space-x-3 items-center p-2">
+          <Image src="/logo.svg" width={32} height={32} className="h-12 w-12 rounded-lg" alt="" />
+
+          <div>
+            <span className="font-semibold text-xl">Github Release Stats</span>
+
+            <div className="text-xs font-semibold">A Ghostbyte Production</div>
+          </div>
+        </div>
       </Link>
-      <div className="text-xs font-semibold">A Ghostbyte Production</div>
     </div>
   );
 };
