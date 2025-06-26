5568df85a9f208ab4506d7af52c1e451901c8efd
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-06-26 20:02:36 +0200

    Add  view on github button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,24 +5,37 @@ const Navbar = () => {
   return (
     <div className="fixed w-full z-20">
       <div className="border-b border-gray-950 py-2 bg-background flex justify-center">
-        <div className="w-full md:w-[90%] px-4 lg:px-8">
-          <Link href="/" className="">
-            <div className="flex space-x-3 items-center p-2">
-              <Image
-                src="/logo.svg"
-                width={48}
-                height={48}
-                className="h-12 w-12 rounded-full"
-                alt=""
-              />
+        <div className="w-full md:w-[90%] px-4 lg:px-8 flex justify-between items-center">
+          <div>
+            <Link href="/" className="">
+              <div className="flex space-x-3 items-center p-2">
+                <Image
+                  src="/logo.svg"
+                  width={48}
+                  height={48}
+                  className="h-12 w-12 rounded-full"
+                  alt=""
+                />
 
-              <div>
-                <span className="font-semibold text-xl">Github Release Stats</span>
+                <div>
+                  <span className="font-semibold text-xl">Github Release Stats</span>
 
-                <div className="text-xs font-semibold">A Ghostbyte Production</div>
+                  <div className="text-xs font-semibold">A Ghostbyte Production</div>
+                </div>
               </div>
-            </div>
-          </Link>
+            </Link>
+          </div>
+
+          <div className="hidden md:block">
+            <Link
+              href="https://github.com/ghostbyte-dev/github-release-stats"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="rounded-md bg-secondary-background px-3 py-1"
+            >
+              View on Github
+            </Link>
+          </div>
         </div>
       </div>
     </div>
