07a3f72116bb2c6cdcec12feafac8be4d2f23ab4
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-07-15 14:59:37 +0200

    add schema.org

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster } from 'react-hot-toast';
 import Footer from '@/components/footer';
 import Providers from './providers';
 import type { Metadata } from 'next/types';
+import Head from 'next/head';
 
 const font = Lexend({
   subsets: ['latin'],
@@ -23,6 +24,39 @@ export const metadata: Metadata = {
   },
 };
 
+function addProductJsonLd() {
+  return {
+    __html: `{
+    "@context": "https://schema.org/",
+    "@type": "SoftwareApplication",
+    "name": "Github Release Stats",
+    "image": [
+      "https://github-release-stats.ghostbyte.dev/schemaImages/homepage.png",
+      "https://github-release-stats.ghostbyte.dev/schemaImages/repositoryPixelix.png",
+      "https://github-release-stats.ghostbyte.dev/schemaImages/repositoryBluesky.png"
+     ],
+    "description": "Github Release Stats is a tool to check the stats of Github repositories and the releases of it. You can check out the downloads history of releases and the star history of repositories.",
+    "applicationCategory": ["Development", "Data Visualization", "Github"],
+    "sameAs": "https://github.com/ghostbyte-dev/github-release-stats",
+    "author": [
+    {
+      "@type": "Organization",
+      "name": "Ghostbyte",
+    },
+    {
+      "@type": "Person",
+      "name": "Daniel Hiebeler",
+    },
+    {
+      "@type": "Person",
+      "name": "Emanuel Hiebeler",
+    },
+  ]
+  }
+`,
+  };
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -30,6 +64,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-bg text-text duration-200" suppressHydrationWarning>
+      <Head>
+        <script
+          type="application/ld+json"
+          // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+          dangerouslySetInnerHTML={addProductJsonLd()}
+          key="product-jsonld"
+        />
+      </Head>
       <body className={`${font.className} antialiased`}>
         <Providers>
           <Navbar />
